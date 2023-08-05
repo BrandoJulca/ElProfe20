e44bad400c71a9cdf35302f541855ff9ce89e7b5
BranDev <brandojulca96@gmail.com>
2023-08-05 16:54:47 -0500

    Subiendo Archivos Locales

---
+++ JavaFinal/src/javafinal/cITEM.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafinal;
+
+/**
+ *
+ * @author Julca PC
+ */
+public class cITEM {
+    // atributos siempre son privados
+    private String nombre;
+    private double precio;
+//------------------------------------------------------------------
+ 
+//---------------------------------------------------------------------
+    
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the precio
+     */
+    public double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    
+    
+    
+        
+}
+   
